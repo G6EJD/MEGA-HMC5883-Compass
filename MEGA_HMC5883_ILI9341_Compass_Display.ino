ca3e51548928ca824dcab98d42acf1f8a10c5ef7
G6EJD <d.l.bird@sky.com>
2022-12-15 16:32:12 +0000

    Update MEGA_HMC5883_ILI9341_Compass_Display.ino

--- MEGA_HMC5883_ILI9341_Compass_Display.ino
+++ MEGA_HMC5883_ILI9341_Compass_Display.ino
@@ -1,15 +1,19 @@
 /*
- * This source code is protected under the terms of the MIT License and is copyright (c) 2013 by David Bird and permission is hereby granted, free of charge, to
- * any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, but not to sub-license and/or 
- * to sell copies of the Software or to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
- *   
- *   See more at http://dsbird.org.uk 
- */   
+  This software, the ideas and concepts is Copyright (c) David Bird 2021 and beyond.
+  All rights to this software are reserved.
+  It is prohibited to redistribute or reproduce of any part or all of the software contents in any form other than the following:
+  1. You may print or download to a local hard disk extracts for your personal and non-commercial use only.
+  2. You may copy the content to individual third parties for their personal use, but only if you acknowledge the author David Bird as the source of the material.
+  3. You may not, except with my express written permission, distribute or commercially exploit the content.
+  4. You may not transmit it or store it in any other website or other form of electronic retrieval system for commercial purposes.
+  5. You MUST include all of this copyright and permission notice ('as annotated') and this shall be included in all copies
+  or substantial portions of the software and where the software use is visible to an end-user.
+  THE SOFTWARE IS PROVIDED "AS IS" FOR PRIVATE USE ONLY, IT IS NOT FOR COMMERCIAL USE IN WHOLE OR PART OR CONCEPT.
+  FOR PERSONAL USE IT IS SUPPLIED WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHOR OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/   
 
 (c) D BIRD 2013
 An Arduino code example for interfacing with the HMC5883
